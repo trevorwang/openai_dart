1d8510b28de5414422376d8a4a1107af83726037
Trevor Wang <trevor.wang@qq.com>
2023-03-20 22:40:43 +0800

    test: chatcompletion request toJson

--- test/chat_completion_test.dart
+++ test/chat_completion_test.dart
@@ -17,5 +17,39 @@ void main() {
         ]
       });
     });
+
+    test('custom chat completion request', () {
+      final request = ChatCompletionRequest(
+        model: Model.gpt3_5Turbo,
+        messages: [
+          ChatMessage(
+              content: "Hello, how are you?", role: ChatMessageRole.system),
+        ],
+        temperature: 0.5,
+        topP: 0.5,
+        n: 1,
+        stream: false,
+        stop: ["<STOP>"],
+        presencePenalty: 1,
+        frequencyPenalty: 1,
+        logitBias: {"HELLO": 1},
+        user: "user",
+      );
+      expect(request.toJson(), {
+        "model": "gpt-3.5-turbo",
+        "messages": [
+          {"content": "Hello, how are you?", "role": "system"}
+        ],
+        "temperature": 0.5,
+        "top_p": 0.5,
+        "n": 1,
+        "stream": false,
+        "stop": ["<STOP>"],
+        "presence_penalty": 1,
+        "frequency_penalty": 1,
+        "logit_bias": {"HELLO": 1},
+        "user": "user",
+      });
+    });
   });
 }
