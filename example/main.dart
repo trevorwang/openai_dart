f5c655b72eef93e0bf309c97361f2b35c43c1aef
Trevor Wang <trevor.wang@qq.com>
2023-03-21 11:10:24 +0800

    feat: add test

--- example/main.dart
+++ example/main.dart
@@ -1,5 +1,18 @@
+import 'package:openai_api/openai_api.dart';
+
 import 'lib/env.dart';
 
 void main() {
-  print(Env.apiKey);
+  final client = OpenaiClient(
+      config: OpenaiConfig(apiKey: Env.apiKey, httpProxy: Env.httpProxy));
+
+  client
+      .sendChatCompletion(ChatCompletionRequest(
+        model: Model.gpt3_5Turbo,
+        messages: [
+          ChatMessage(content: "hello", role: ChatMessageRole.user),
+        ],
+      ))
+      .then((value) => print(value))
+      .catchError((err) => print(err));
 }
