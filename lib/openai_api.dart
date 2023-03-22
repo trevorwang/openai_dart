4a6ab544cdde6d7c026dc346dc268db2e1b1880f
Trevor Wang <trevor.wang@qq.com>
2023-03-22 14:48:48 +0800

    feat: audio transcription

--- lib/openai_api.dart
+++ lib/openai_api.dart
@@ -3,4 +3,6 @@ library openai_api;
 export 'src/config.dart';
 export 'src/constants.dart';
 export 'src/client.dart';
+export 'src/errors.dart';
 export 'src/chat/completion.dart';
+export 'src/audio/transcription.dart';
---
+++ test/audio_trasription_test.dart
@@ -0,0 +1,41 @@
+import 'package:http/http.dart';
+import 'package:http/testing.dart';
+import 'package:openai_api/openai_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('audio transcription', () {
+    test('request object default value', () {
+      final req = TranscriptionRequest(
+        file: "assets/ttsmaker-file-2023-3-22-14-2-35.mp3",
+      );
+
+      expect(req.model, Model.whisper_1);
+      expect(req.toJson(), {
+        "file": "assets/ttsmaker-file-2023-3-22-14-2-35.mp3",
+        "model": "whisper-1",
+      });
+    });
+
+    // test http request with http client
+    test("http request", () async {
+      final client = OpenaiClient(
+          config: OpenaiConfig(apiKey: "sk-1234567890"),
+          httpClient: MockClient(
+            (request) {
+              return Future.value(
+                Response('{"text":"hello world"}', 200),
+              );
+            },
+          ));
+      final result = await client.createTrascription(TranscriptionRequest(
+        file: "assets/ttsmaker-file-2023-3-22-14-2-35.mp3",
+      ));
+      expect(
+        result,
+        isA<TrascriptionResponse>().having(
+            (p0) => p0.text, "convert text is not correct", "hello world"),
+      );
+    });
+  });
+}
